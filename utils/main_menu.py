acc5349ae042f3bb59475130bef21333561ece58
YuryRass <yurchik1997yurchik@gmail.com>
2023-10-26 23:03:23 +0300

    add check of user's full names

--- utils/main_menu.py
+++ utils/main_menu.py
@@ -4,8 +4,9 @@ from lexicon import LEXICON_COMMANDS_RU
 
 
 async def set_main_menu(bot: Bot) -> None:
-    commands: list[BotCommand] = [BotCommand(command=command,
-                                             description=description)
-                                  for command, description
-                                  in LEXICON_COMMANDS_RU.items()]
+    commands: list[BotCommand] = [
+        BotCommand(
+            command=command, description=description
+        ) for command, description in LEXICON_COMMANDS_RU.items()
+    ]
     await bot.set_my_commands(commands=commands)
