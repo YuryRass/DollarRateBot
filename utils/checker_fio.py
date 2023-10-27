1a1ae8a4d7f2b8589f6041cab70bca17e23f548c
YuryRass <yurchik1997yurchik@gmail.com>
2023-10-27 23:24:34 +0300

    Check of user's full name

---
+++ utils/checker_fio.py
@@ -0,0 +1,9 @@
+import re
+
+
+def is_correct_fullname(full_name: str) -> bool:
+    fio_pattern: re.Pattern = re.compile(
+        r'^[А-ЯЁA-Z][а-яёa-z]+\s+[А-ЯЁA-Z][а-яёa-z]+\s+[А-ЯЁA-Z][а-яёa-z]'
+    )
+
+    return fio_pattern.match(full_name)
