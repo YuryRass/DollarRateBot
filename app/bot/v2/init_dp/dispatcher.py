d02b2c5998a53be5f845d3dc57143b4c9fd7318b
Yurii <yurchik1997yurchik@gmail.com>
2025-04-19 14:20:46 +0300

    refactoring

--- app/bot/v2/init_dp/dispatcher.py
+++ app/bot/v2/init_dp/dispatcher.py
@@ -5,9 +5,14 @@ from aiogram_dialog import setup_dialogs
 
 from app.bot.v2.dollar_rate.dialog import dollar_rate_dialog
 from app.bot.v2.dollar_rate.handlers.handlers import router as start_router
+from app.bot.v2.nats_storage.storage import NatsStorage
 
-dispatcher: Dispatcher = Dispatcher()
 
-dispatcher.include_router(start_router)
-dispatcher.include_router(dollar_rate_dialog)
-setup_dialogs(dispatcher)
+def get_dispatcher(storage: NatsStorage) -> Dispatcher:
+    dispatcher: Dispatcher = Dispatcher(storage=storage)
+
+    dispatcher.include_router(start_router)
+    dispatcher.include_router(dollar_rate_dialog)
+    setup_dialogs(dispatcher)
+
+    return dispatcher
