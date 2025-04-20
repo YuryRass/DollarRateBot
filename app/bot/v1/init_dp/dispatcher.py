ad39e8663f01dc2d225734d69331b6f6eb8dc06a
Yurii <yurchik1997yurchik@gmail.com>
2025-04-20 23:11:08 +0300

    refactoring

--- app/bot/v1/init_dp/dispatcher.py
+++ app/bot/v1/init_dp/dispatcher.py
@@ -2,21 +2,8 @@
 
 from aiogram import Dispatcher
 
-import app.bot.v1.handlers.cancel_subscription as cancel_hdr
-import app.bot.v1.handlers.delete_account as del_account_hdr
-import app.bot.v1.handlers.dollar_price as dollar_hdr
-import app.bot.v1.handlers.other_handlers as other_hdrs
-import app.bot.v1.handlers.registry_user as reg_hdr
-import app.bot.v1.handlers.user_history as history_hdr
-import app.bot.v1.handlers.user_subscription as subscr_hdr
+from app.bot.v1.init_dp.routers import get_routers
 
 dispatcher: Dispatcher = Dispatcher()
 
-
-dispatcher.include_router(other_hdrs.router)
-dispatcher.include_router(dollar_hdr.router)
-dispatcher.include_router(reg_hdr.router)
-dispatcher.include_router(history_hdr.router)
-dispatcher.include_router(subscr_hdr.router)
-dispatcher.include_router(cancel_hdr.router)
-dispatcher.include_router(del_account_hdr.router)
+dispatcher.include_routers(*get_routers())
