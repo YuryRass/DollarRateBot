0aeaaf01ecd4e51857baa442ef29115f62ab7285
Yurii <yurchik1997yurchik@gmail.com>
2025-04-22 22:59:01 +0300

    refactoring

--- tests/bot/v1/handlers/test_other_handlers.py
+++ tests/bot/v1/handlers/test_other_handlers.py
@@ -14,21 +14,24 @@ from tests.bot.v1.helper import create_tg_command_message
 
 @pytest.mark.parametrize("tg_command", ["/help", "/start"])
 @pytest.mark.asyncio
-async def test_basic_commands(tg_command: str, dp: Dispatcher, mock_bot: MockedBot) -> None:
-    assert not mock_bot.session.requests
-    user_id = 1234567
-    message = create_tg_command_message(tg_command, user_id)
+async def test_basic_commands(
+    tg_command: str, dp: Dispatcher, mock_bot_with_message: MockedBot
+) -> None:
+    # assert not mock_bot_with_message.session.requests
+    message = create_tg_command_message(tg_command)
 
     # пользователь не зареган
     assert not await UserService.is_user_registered(user_tg_id=message.from_user.id)
 
-    result = await dp.feed_update(mock_bot, Update(message=message, update_id=1))
+    result = await dp.feed_update(mock_bot_with_message, Update(message=message, update_id=1))
     assert result is not UNHANDLED
-    outgoing_message: TelegramType = mock_bot.get_request()
+    outgoing_message: TelegramType = mock_bot_with_message.get_request()
     assert isinstance(outgoing_message, SendMessage)
 
     expected_text = (
-        START_MESSAGE.format(user_full_name=message.from_user.full_name, msg=LEXICON[tg_command])
+        START_MESSAGE.format(
+            user_full_name=message.from_user.full_name, msg=LEXICON[tg_command]
+        )
         if tg_command == "/start"
         else LEXICON[tg_command]
     )
@@ -36,19 +39,23 @@ async def test_basic_commands(tg_command: str, dp: Dispatcher, mock_bot: MockedB
 
 
 @pytest.mark.asyncio
-async def test_start_command_for_registered_user(registered_user, dp: Dispatcher, mock_bot: MockedBot) -> None:
-    assert not mock_bot.session.requests
-    user_id = 1234567
-    message = create_tg_command_message("/start", user_id)
+async def test_start_command_for_registered_user(
+    registered_user, dp: Dispatcher, mock_bot_with_message: MockedBot
+) -> None:
+    # assert not mock_bot_with_message.session.requests
+    message = create_tg_command_message("/start")
 
     # пользователь зареган
     assert await UserService.is_user_registered(user_tg_id=message.from_user.id)
 
-    result = await dp.feed_update(mock_bot, Update(message=message, update_id=1))
+    result = await dp.feed_update(mock_bot_with_message, Update(message=message, update_id=1))
     assert result is not UNHANDLED
-    outgoing_message: TelegramType = mock_bot.get_request()
+    outgoing_message: TelegramType = mock_bot_with_message.get_request()
     assert isinstance(outgoing_message, SendMessage)
     assert outgoing_message.text == (
-        START_MESSAGE.format(user_full_name=message.from_user.full_name, msg=LEXICON["/start"])
+        START_MESSAGE.format(
+            user_full_name=message.from_user.full_name,
+            msg=LEXICON["/start"],
+        )
     )
     assert outgoing_message.reply_markup == get_main_keyboard("register")
