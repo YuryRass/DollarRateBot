e7c889980db0c42950e6cb5b14d5528a90647094
Yurii <yurchik1997yurchik@gmail.com>
2025-04-22 23:00:17 +0300

    added dollar price fixtures

--- tests/bot/v1/conftest.py
+++ tests/bot/v1/conftest.py
@@ -1,20 +1,36 @@
-from unittest.mock import patch
+from unittest.mock import AsyncMock, patch
 
 import pytest
 from aiogram import Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.methods import SendMessage
 
+from app.api_requests.request import DollarConverter
 from app.bot.v1.init_dp.routers import get_routers
 from app.database.database import Base
 from tests.bot.mocked_aiogram import MockedBot, MockedSession
 from tests.bot.v1.database import test_async_engine, test_scoped_session
 from tests.bot.v1.factories import DollarHistoryFactory, UserFactory
+from tests.bot.v1.helper import DOLLAR_IN_RUB
 
 
 @pytest.fixture(autouse=True)
 def mock_db_session():
     with patch("app.database.database.async_session", new=test_scoped_session):
+        yield
+
+
+@pytest.fixture
+def dollar_in_rub() -> float:
+    return DOLLAR_IN_RUB
+
+
+@pytest.fixture(autouse=True)
+def mock_dollar_price(dollar_in_rub: float):
+    with patch.object(
+        DollarConverter, "get_price", new_callable=AsyncMock
+    ) as mock_method:
+        mock_method.return_value = dollar_in_rub
         yield
 
 
@@ -51,6 +67,6 @@ def bot() -> MockedBot:
 
 
 @pytest.fixture
-def mock_bot(bot: MockedBot) -> MockedBot:
+def mock_bot_with_message(bot: MockedBot) -> MockedBot:
     bot.add_result_for(method=SendMessage, ok=True)
     return bot
